6e04c3ba9cbd8f1ec2be71a629c62e10463b0441
Seth Tippetts <sethtippetts@gmail.com>
2015-05-01 13:07:47 -0600

    Trying out promise pooling. Probably doesn't work

--- es5/index.js
+++ es5/index.js
@@ -5,7 +5,8 @@ var _classCallCheck = function (instance, Constructor) { if (!(instance instance
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
 var jsforce = require('jsforce'),
-    Promise = require('bluebird');
+    Promise = require('bluebird'),
+    _ = require('lodash');
 
 var oneHourMillis = 1000 * 60 * 60;
 
@@ -16,12 +17,135 @@ module.exports = (function () {
     if (!config || !config.Endpoint || !config.SecurityToken || !config.Password || !config.Username) {
       throw new Error('Missing SalesForce credentials');
     }
-    this.config = JSON.parse(JSON.stringify(config));
+    this.connectionCount = 0;
+    this.config = _.extend({
+      min: 1,
+      max: 8,
+      timeout: oneHourMillis
+    }, config);
     this.pool = [];
+    this.queue = [];
   }
 
   _createClass(VSForce, [{
+    key: 'acquire',
+
+    /**
+     * PoolParty.acquire
+     * @return {Disposer} [description]
+     */
+    value: function acquire() {
+
+      // No available connections
+      if (!this.pool.length) {
+
+        // Creating new connection
+        if (this.connectionCount < this.config.max) {
+          return this.create();
+        } // Maxed out connections, adding to queue
+        return this.queue();
+      }
+
+      // Get first pool connection
+      var resource = this.pool.shift();
+
+      // If it's valid, use that connection
+      if (this.isValid(resource)) {
+        return resource;
+      } // If it's not, recurse (pool length should be shorter)
+      return this.aquire();
+    }
+  }, {
+    key: 'create',
+    value: function create() {
+      var _this = this;
+
+      console.log('Creating new connection');
+      this.conn = new jsforce.Connection({
+        loginUrl: this.config.Endpoint,
+        accessToken: this.config.SecurityToken
+      });
+
+      this.conn.query = Promise.promisify(this.conn.query, this.conn);
+      this.conn.login = Promise.promisify(this.conn.login, this.conn);
+
+      return this.conn.login(this.config.Username, this.config.Password + this.config.SecurityToken).then(function () {
+
+        for (var prop in _this.conn) {
+          console.log('PROPERTY', prop);
+        }
+        _this.conn._initializedAt = Date.now();
+        _this.connectionCount++;
+        return _this.conn;
+      }).disposer(function (conn) {
+        return _this.release(conn);
+      });
+    }
+  }, {
+    key: 'queue',
+    value: function queue() {
+      return new Promise(function (resolve, reject) {
+        this.queue.push({ resolve: resolve, reject: reject });
+      });
+    }
+  }, {
+    key: 'release',
+    value: function release(conn) {
+      console.log('Releasing connection');
+
+      // No waiting connections. Release to the pool.
+      if (!this.queue.length) {
+        return this.pool.push(conn);
+      } // Resolve waiting promise with last connection
+      var promise = this.queue.shift();
+      promise.resolve(conn);
+    }
+  }, {
+    key: 'destroy',
+    value: function destroy(conn) {
+      var _this2 = this;
+
+      console.log('Destroying connection');
+      return new Promise(function (resolve, reject) {
+        _this2.conn.logout(function (err) {
+          if (err) return reject(err);
+          _this2.connectionCount--;
+          resolve(true);
+        });
+      });
+    }
+  }, {
+    key: 'isValid',
+    value: function isValid(conn) {
+      var dur = Date.now() - conn._initializedAt;
+
+      console.log('Duration of connection: ' + calcDur + ' hours');
+
+      // Connection still valid, don't renew
+      if (dur > this.config.timeout) {
+        this.destroy(conn);
+        return false;
+      }
+      return true;
+    }
+  }, {
+    key: 'getConnection',
+
+    /**
+     * getConnection
+     * @returns {promise|*|Function|promise|promise|promise}
+     */
+    value: function getConnection() {
+      return Promise.using(this.aquire());
+    }
+  }, {
     key: 'query',
+
+    /**
+     * PoolParty.query
+     * @param  {SOQL Query} arg
+     * @return {Promise}
+     */
     value: function query(arg) {
       return this._wrap('query', arg);
     }
@@ -35,64 +159,6 @@ module.exports = (function () {
     value: function _wrap(fnName, arg) {
       return this.getConnection().then(function (conn) {
         return conn[fnName](arg);
-      });
-    }
-  }, {
-    key: 'getConnection',
-
-    /**
-     * getConnection
-     * @returns {promise|*|Function|promise|promise|promise}
-     */
-    value: function getConnection() {
-      var _this = this;
-
-      return this.checkConnDuration().then(function (renew) {
-        if (!renew && !_this.config.newConn && _this.conn.loginUrl === _this.config.Endpoint) return Promise.resolve(_this.conn);
-        _this.config.newConn = false;
-
-        _this.conn = new jsforce.Connection({
-          loginUrl: _this.config.Endpoint,
-          accessToken: _this.config.SecurityToken
-        });
-
-        _this.conn.query = Promise.promisify(_this.conn.query, _this.conn);
-        _this.conn.login = Promise.promisify(_this.conn.login, _this.conn);
-
-        return _this.conn.login(_this.config.Username, _this.config.Password + _this.config.SecurityToken).then(function () {
-          _this.conn._initializedAt = Date.now();
-          return _this.conn;
-        });
-      });
-    }
-  }, {
-    key: 'checkConnDuration',
-
-    /**
-     * checkConnDuration
-     * @param conn
-     */
-    value: function checkConnDuration() {
-      var _this2 = this;
-
-      // No connection, create one.
-      if (!this.conn) {
-        return Promise.resolve(true);
-      }var dur = Date.now() - this.conn._initializedAt,
-          calcDur = dur / oneHourMillis;
-
-      console.log('Duration of connection: ' + calcDur + ' hours');
-
-      // Connection still valid, don't renew
-      if (calcDur < 10) {
-        return Promise.resolve(false);
-      }return new Promise(function (resolve, reject) {
-        console.log('Logging out connection at: ' + calcDur);
-        _this2.conn.logout(function (err) {
-          if (err) return reject(err);
-          console.log('Session has been expired.');
-          resolve(true);
-        });
       });
     }
   }]);
--- es6/index.js
+++ es6/index.js
@@ -1,16 +1,130 @@
 var jsforce = require('jsforce')
-  , Promise = require('bluebird');
+  , Promise = require('bluebird')
+  , _ = require('lodash');
 
 var oneHourMillis = 1000 * 60 * 60;
 
 module.exports = class VSForce {
+
   constructor(config) {
     if(!config || !config.Endpoint || !config.SecurityToken || !config.Password || !config.Username){
       throw new Error('Missing SalesForce credentials');
     }
-    this.config = JSON.parse(JSON.stringify(config));
+    this.connectionCount = 0;
+    this.config = _.extend({
+      min: 1,
+      max: 8,
+      timeout: oneHourMillis
+    }, config);
     this.pool = [];
+    this.queue = [];
   }
+
+  /**
+   * PoolParty.acquire
+   * @return {Disposer} [description]
+   */
+  acquire(){
+
+    // No available connections
+    if(!this.pool.length){
+
+      // Creating new connection
+      if(this.connectionCount < this.config.max) return this.create();
+
+      // Maxed out connections, adding to queue
+      return this.queue();
+    }
+
+    // Get first pool connection
+    var resource = this.pool.shift();
+
+    // If it's valid, use that connection
+    if(this.isValid(resource)) return resource;
+
+    // If it's not, recurse (pool length should be shorter)
+    return this.aquire();
+  }
+
+  create() {
+
+    console.log('Creating new connection');
+    this.conn = new jsforce.Connection({
+      loginUrl: this.config.Endpoint,
+      accessToken: this.config.SecurityToken
+    });
+
+    this.conn.query = Promise.promisify(this.conn.query, this.conn);
+    this.conn.login = Promise.promisify(this.conn.login, this.conn);
+
+    return this.conn.login(this.config.Username, this.config.Password + this.config.SecurityToken)
+      .then(() => {
+
+        for(var prop in this.conn){
+          console.log("PROPERTY", prop);
+        }
+        this.conn._initializedAt = Date.now();
+        this.connectionCount++;
+        return this.conn;
+      })
+      .disposer((conn) => this.release(conn));
+  }
+
+  queue(){
+    return new Promise(function(resolve, reject){
+      this.queue.push({resolve,reject});
+    });
+  }
+
+  release(conn){
+    console.log('Releasing connection');
+
+    // No waiting connections. Release to the pool.
+    if(!this.queue.length) return this.pool.push(conn);
+
+    // Resolve waiting promise with last connection
+    var promise = this.queue.shift();
+    promise.resolve(conn);
+  }
+
+  destroy(conn){
+    console.log('Destroying connection');
+    return new Promise((resolve, reject) => {
+      this.conn.logout((err) =>  {
+        if (err) return reject(err);
+        this.connectionCount--;
+        resolve(true);
+      });
+    });
+  }
+
+  isValid(conn){
+    var dur = Date.now() - conn._initializedAt;
+
+    console.log('Duration of connection: '+calcDur+' hours');
+
+    // Connection still valid, don't renew
+    if(dur>this.config.timeout){
+      this.destroy(conn)
+      return false;
+    }
+    return true;
+  }
+
+
+  /**
+   * getConnection
+   * @returns {promise|*|Function|promise|promise|promise}
+   */
+  getConnection() {
+    return Promise.using(this.aquire());
+  }
+
+  /**
+   * PoolParty.query
+   * @param  {SOQL Query} arg
+   * @return {Promise}
+   */
   query(arg) {
     return this._wrap('query', arg);
   }
@@ -20,58 +134,5 @@ module.exports = class VSForce {
   _wrap(fnName, arg) {
     return this.getConnection()
       .then((conn) => conn[fnName](arg));
-  }
-
-  /**
-   * getConnection
-   * @returns {promise|*|Function|promise|promise|promise}
-   */
-  getConnection() {
-    return this.checkConnDuration()
-      .then((renew) => {
-        if(!renew &&!this.config.newConn&&this.conn.loginUrl===this.config.Endpoint) return Promise.resolve(this.conn);
-        this.config.newConn=false;
-
-        this.conn = new jsforce.Connection({
-          loginUrl: this.config.Endpoint,
-          accessToken: this.config.SecurityToken
-        });
-
-        this.conn.query = Promise.promisify(this.conn.query, this.conn);
-        this.conn.login = Promise.promisify(this.conn.login, this.conn);
-
-        return this.conn.login(this.config.Username, this.config.Password + this.config.SecurityToken)
-          .then(() => {
-            this.conn._initializedAt = Date.now();
-            return this.conn;
-          });
-      });
-  }
-
-
-  /**
-   * checkConnDuration
-   * @param conn
-   */
-  checkConnDuration(){
-    // No connection, create one.
-    if(!this.conn) return Promise.resolve(true);
-
-    var dur = Date.now() - this.conn._initializedAt
-      , calcDur = dur/oneHourMillis;
-
-    console.log('Duration of connection: '+calcDur+' hours');
-
-    // Connection still valid, don't renew
-    if(calcDur<10) return Promise.resolve(false);
-
-    return new Promise((resolve, reject) => {
-      console.log('Logging out connection at: '+calcDur);
-      this.conn.logout((err) =>  {
-        if (err) return reject(err);
-        console.log('Session has been expired.');
-        resolve(true);
-      });
-    });
   }
 }
